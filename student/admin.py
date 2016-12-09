3b71aecd883a5472eae657a0c7a556a123d42b43
hanyeluoxue <hanyeluoxue@163.com>
2016-12-09 10:19:36 +0800

    register model student/referral

--- student/admin.py
+++ student/admin.py
@@ -6,4 +6,15 @@ from student import models
 class StuAdmin(admin.ModelAdmin):
     raw_id_fields = ('stu_name',)
 
+
+class ReferralAdmin(admin.ModelAdmin):
+    search_fields = ('qq','referralfrom__stu_name__qq')
+    def get_actions(self, request):
+        actions = super(ReferralAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
 admin.site.register(models.StuAccount)
+admin.site.register(models.Referral, ReferralAdmin)
--- student/models.py
+++ student/models.py
@@ -24,3 +24,11 @@ class Referral(models.Model):
     comment = models.TextField(u"备注",help_text=u"推荐的同学的大体状况，比如学习意愿，文化水平等等")
     consultant = models.ForeignKey(UserProfile,verbose_name=u"课程顾问")
     referral_date = models.DateTimeField(auto_now_add=True,auto_created=True,verbose_name="推荐日期")
+
+    class Meta:
+        verbose_name='学员推荐表'
+        verbose_name_plural = '学员推荐表'
+
+    def __str__(self):
+
+        return  '%s (%s)' % (self.qq,self.name)
